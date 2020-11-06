fee1cda6faacf7a68ff569f6211c880a41ef9bb9
chengjun <wangchj04@gmail.com>
2020-11-06 17:37:48 +0800

    :art:

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,6 @@
 # cd ..
 # jupyter-book build mybook/
 # cd mybook
-
 # Publish your book's HTML manually to GitHub-pages
 # publish the _site folder of the master branch to the gh-pages branch
 ghp-import -n -p -f _build/html
